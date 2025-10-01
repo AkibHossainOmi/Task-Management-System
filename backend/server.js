a38817eb5fe07d8abb9f54a13b12b50c79fa1a66
AkibHossainOmi <akibhossainomi2000@gmail.com>
2025-10-01 14:05:29 +0600

    feat: added swagger docs and made the cors permission configurable.

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,11 @@ const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
 const connectDB = require('./config/db');
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_URL }));
 app.use(express.json());
 
 connectDB(process.env.MONGO_URI);
@@ -27,6 +29,9 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/tasks', require('./routes/tasks'));
+
+const swaggerDocument = YAML.load('./docs/swagger.yaml');
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
